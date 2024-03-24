c05137d75e9d51fbac9dec1ca9c4334e8f8f2754
Alvis257 <alvispastars@inbox.lv>
2024-03-24 11:47:05 +0200

    Added menue

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BlankFormComponent } from './blank-form/blank-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'mani-dokumenti', component: BlankFormComponent },
+  { path: 'visi-dokumenti', component: BlankFormComponent },
+  { path: 'datu-kopas', component: BlankFormComponent },
+  { path: 'integrācijas', component: BlankFormComponent },
+  { path: 'profils', component: BlankFormComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Angular/src/app/blank-form/blank-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blank-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './blank-form.component.html',
+  styleUrl: './blank-form.component.scss'
+})
+export class BlankFormComponent {
+
+}
